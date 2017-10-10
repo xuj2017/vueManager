3e05dd75f8368a7c7ecbcff7d57c9f59f7aa717f
xujun <xuj2017@outlook.com>
2017-10-10 17:43:44 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="home">
+        <headers></headers>
+        <div class="meau">
+            <meau></meau>
+        </div>
+    </div>
+</template>
+<script>
+
+import headers from './header/Header'
+import meau from './meau/Meau'
+export default {
+    components: {
+        headers,meau
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    .meau{
+        width: 200px;
+       height: 100%;
+    }
+    #home{
+        width: 100%;
+        height: 100%;
+    }
+</style>
